dbcb93ca529005f80c0dcd80a3b07244d73420e2
David <Dankerprouduct2@gmail.com>
2015-10-02 22:18:31 -0400

    Update Method

--- Game Engine/Form1.cs
+++ Game Engine/Form1.cs
@@ -12,9 +12,20 @@ namespace Game_Engine
 {
     public partial class Form1 : Form
     {
+        Timer timer; 
         public Form1()
         {
             InitializeComponent();
+
+            timer = new Timer();
+            timer.Interval = 20;
+            timer.Enabled = true;
+            timer.Tick += new EventHandler(Update); 
+        }
+
+        void Update(object source, EventArgs e)
+        {
+            Console.WriteLine("update"); 
         }
     }
 }
