b756f50124dbec4dea3637f5d12f0da43d97288a
David <Dankerprouduct2@gmail.com>
2015-10-02 22:34:15 -0400

    Start Method and other junk

--- Game Engine/Form1.cs
+++ Game Engine/Form1.cs
@@ -12,20 +12,51 @@ namespace Game_Engine
 {
     public partial class Form1 : Form
     {
-        Timer timer; 
+        Timer timer;
+        PictureBox pictureBox;
+        Bitmap surface;
+        Graphics graphics;
+
+        string version; 
         public Form1()
         {
             InitializeComponent();
 
+            this.Size = new Size(600, 500);
+
             timer = new Timer();
             timer.Interval = 20;
             timer.Enabled = true;
-            timer.Tick += new EventHandler(Update); 
+            timer.Tick += new EventHandler(Update);
+
+            pictureBox = new PictureBox();
+            pictureBox.Parent = this;
+            pictureBox.Dock = DockStyle.Fill;
+            pictureBox.BackColor = Color.SkyBlue;
+
+            surface = new Bitmap(this.Size.Width, this.Size.Height);
+            pictureBox.Image = surface;
+            graphics = Graphics.FromImage(surface);
+
+
+            version = "1.00";
+
+            Start(); 
+
         }
 
+        void Start()
+        {
+            Font font = new Font("Ariel", 14, FontStyle.Regular, GraphicsUnit.Point);
+            graphics.DrawString(version, font, Brushes.Black, 5, 5); 
+ 
+        }
+
+        int i;
         void Update(object source, EventArgs e)
         {
-            Console.WriteLine("update"); 
+            Console.WriteLine("update " + i);
+            i++; 
         }
     }
 }
