f1f98b5c749b427f09e9fd36c96f814f25a76598
David <Dankerprouduct2@gmail.com>
2015-10-04 13:21:08 -0400

    Finished the Sprite class

--- Game Engine/Sprite.cs
+++ Game Engine/Sprite.cs
@@ -7,7 +7,7 @@ namespace Game_Engine
     class Sprite
     {
 
-        public enum AnimateDirection
+        public enum AnimateDir
         {
             None = 0,
             Forward = 1,
@@ -23,13 +23,13 @@ namespace Game_Engine
         private Cipher game;
         private PointF position;
         private PointF velocity;
-        private Size size;
+        private Size mSize;
         private Bitmap bitmap;
         private bool alive;
         private int colums;
         private int totalFrames;
         private int currentFrame;
-        private AnimateDirection animateDirection;
+        private AnimateDir animateDirection;
         private AnimateWrap animateWrap;
         private int lastTime;
         private int animationRate;
@@ -41,13 +41,13 @@ namespace Game_Engine
             position = new PointF(0, 0);
 
             velocity = new PointF(0, 0);
-            size = new Size(0, 0);
+            mSize = new Size(0, 0);
             bitmap = null;
             alive = true;
             colums = 1;
             totalFrames = 1;
             currentFrame = 0;
-            animateDirection = AnimateDirection.Forward;
+            animateDirection = AnimateDir.Forward;
             animateWrap = AnimateWrap.Wrap;
             lastTime = 0;
             animationRate = 30; 
@@ -120,6 +120,183 @@ namespace Game_Engine
                 position.Y = value;
             }
         }
+        public Size size
+        {
+            get
+            {
+                return mSize;
+            }
+            set
+            {
+                mSize = value;
+            }
+        }
+        public int Width
+        {
+            get
+            {
+                return mSize.Width;
+            }
+            set
+            {
+                mSize.Width = value;
+            }
+        }
+        public int Height
+        {
+            get
+            {
+                return mSize.Height;
+            }
+            set
+            {
+                mSize.Height = value;
+            }
+        }
+        public int Columns
+        {
+            get
+            {
+                return colums;
+            }
+            set
+            {
+                colums = value; 
+            }
+        }
+        public int TotalFrames
+        {
+            get
+            {
+                return totalFrames;
+            }
+            set
+            {
+                totalFrames = value;
+            }
+        }
+        public int CurrentFrame
+        {
+            get
+            {
+                return currentFrame;
+            }
+            set
+            {
+                currentFrame = value;
+            }
+        }
+
+        public AnimateDir AnimateDirection
+        {
+            get
+            {
+                return animateDirection;
+            }
+            set
+            {
+                animateDirection = value;
+            }
+        }
+        public AnimateWrap AnimateWrapMode
+        {
+            get
+            {
+                return animateWrap;
+            }
+            set
+            {
+                animateWrap = value;
+            }
+        }
+        public int AnimationRate
+        {
+            get
+            {
+                return (1000 / animationRate);
+            }
+            set
+            {
+                if (value == 0)
+                {
+                    value = 1;
+                }
+                animationRate = 1000 / value; 
+            }
+        }
+        public void Animate()
+        {
+            Animate(0, totalFrames - 1); 
+        }
+        public void Animate(int startFrame, int endFrame)
+        {
+            if (totalFrames <= 0)
+            {
+                return;
+            }
+
+            int time = Environment.TickCount;
+            if (time > lastTime + animationRate)
+            {
+                lastTime = time;
+
+                currentFrame += (int)animateDirection;
+                switch (animateWrap)
+                {
+                    case AnimateWrap.Wrap:
+                        {
+                            if (currentFrame < startFrame)
+                            {
+                                currentFrame = endFrame; 
+                            }
+                            else if (currentFrame > endFrame)
+                            {
+                                currentFrame = startFrame;
+                            }
+                            break;
+                        }
+                    case AnimateWrap.Bounce:
+                        {
+                            if (currentFrame < startFrame)
+                            {
+                                currentFrame = startFrame;
+                                animateDirection = AnimateDir.Forward;
+                            }
+                            else if (currentFrame > endFrame)
+                            {
+                                currentFrame = endFrame;
+                                animateDirection = AnimateDir.Backward; 
+                            }
+                            break;
+                        }
+                }
+            }
+        }
+
+        public void Draw()
+        {
+            Rectangle frame = new Rectangle();
+            frame.X = (currentFrame % colums) * size.Width;
+            frame.Y = (currentFrame / colums) * size.Height;
+            frame.Width = size.Width;
+            frame.Height = size.Height;
+            game.Device.DrawImage(bitmap, Bounds, frame, GraphicsUnit.Pixel);
+        }
+        public Rectangle Bounds
+        {
+            get
+            {
+                Rectangle rect = new Rectangle((int)position.X, (int)position.Y, size.Width, size.Height);
+                return rect;
+            }
+            
+        }
+        public bool IsColliding(ref Sprite other)
+        {
+            bool collision = Bounds.IntersectsWith(other.Bounds);
+            return collision; 
+        }
+        
     }
 
 }
