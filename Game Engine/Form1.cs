14854ae6d2050adca37909301a7b6899e390fc05
David <Dankerprouduct2@gmail.com>
2015-10-04 14:19:04 -0400

    Work on Gametime and key presses

--- Game Engine/Form1.cs
+++ Game Engine/Form1.cs
@@ -7,40 +7,77 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using CipherEngine; 
+
+using CipherEngine;
+
 
 namespace Game_Engine
 {
     public partial class Form1 : Form
     {
-        public Cipher game; 
-        public Bitmap picture; 
+        private Cipher game; 
         public Form1()
         {
             InitializeComponent();
-            this.Text = "Cipher Game Engine";
-
-            game = new Cipher(this, 600, 500); 
-
             
-            picture = game.LoadBitmap(@"C:\Users\David\Pictures\Screenshots\Screenshot (203).png");
-            if (picture == null)
-            {
-                MessageBox.Show("Error loading picture");
-                Environment.Exit(0);
-            }
-
-            game.Device.DrawImage(picture, 10, 10);
-            game.Update(); 
         }
-
+        public void Form1_Load(object sender, EventArgs e)
+        {
+            Main(); 
+        }
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            Game_KeyPressed(e.KeyCode); 
+        }
         public void Form1_FormClosed(object sender, FormClosedEventArgs e)
         {
-            game = null; 
+            
         }
 
-        
+        public bool GameInitialize()
+        {
+            this.Text = "Cipher Game Engine";
+            return true;
+        }
+        public void Main()
+        {
+            Form form = (Form)this;
+            game = new Cipher(form, 800, 600);
 
+            GameInitialize();
+
+            while (true)
+            {
+                
+            }
+        }
+
+        public void Game_KeyPressed(System.Windows.Forms.Keys key)
+        {
+            switch (key)
+            {
+                case Keys.Up:
+                    {
+
+                        break;
+                    }
+                case Keys.Down:
+                    {
+
+                        break;
+                    }
+                case Keys.Left:
+                    {
+
+                        break;
+                    }
+                case Keys.Right:
+                    {
+
+                        break;
+                    }
+            }
+        }
         
 
 
